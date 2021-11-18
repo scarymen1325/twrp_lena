5389f3b53ba85b6a850509c6a59e45eaf03710b2
Aarqw12 <lcockx@protonmail.com>
2021-11-18 13:03:36 -0500

    initial devices trees

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_pdx213.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_pdx213-user \
+    omni_pdx213-userdebug \
+    omni_pdx213-eng
